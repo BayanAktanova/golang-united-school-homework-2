a801d086884aabe6b28154409506840eb15da8cb
Bayan <aktanova_b@bi.group>
2022-02-27 19:00:53 +0600

    Homework 2

--- solution.go
+++ solution.go
@@ -4,6 +4,14 @@ import (
 	"math"
 )
 
+type sides int
+
+const (
+	SidesCircle   sides = 0
+	SidesTriangle sides = 3
+	SidesSquare   sides = 4
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -12,7 +20,7 @@ import (
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum int) float64 {
+func CalcSquare(sideLen float64, sidesNum sides) float64 {
 
 	if sidesNum == 3 {
 		return math.Sqrt(3) / 4 * sideLen * sideLen
